f081c8c42435d4c4f7175af4ebba3f1c07a96924
treycluffIRM <treycluff@gmail.com>
2023-08-29 21:44:26 -0400

    making server cors available

--- main.js
+++ main.js
@@ -8,7 +8,32 @@ const maxInterval = 3000;
 const minInterval = 1000;
 
 const client = require("redis").createClient(); //database
-const http = require("http").createServer(); //web server
+const http = require("http");
+const port = 3000;
+
+http.createServer((req, res) => {
+	const headers = {
+		"Access-Control-Allow-Origin": "*" /* @dev First, read about security */,
+		"Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+		"Access-Control-Max-Age": 2592000, // 30 days
+		/** add other headers as per requirement */
+	};
+
+	if (req.method === "OPTIONS") {
+		res.writeHead(204, headers);
+		res.end();
+		return;
+	}
+
+	if (["GET", "POST"].indexOf(req.method) > -1) {
+		res.writeHead(200, headers);
+		res.end("Hello World");
+		return;
+	}
+
+	res.writeHead(405, headers);
+	res.end(`${req.method} is not allowed for the request.`);
+}).listen(port);
 
 let interval = maxInterval;
 let timer;
