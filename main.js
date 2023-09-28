db7577740719a06aacbc089eb3484dae9654dedf
Trey <treycluff@gmail.com>
2023-09-28 16:40:52 -0400

    added age check

--- main.js
+++ main.js
@@ -27,8 +27,12 @@ async function init() {
 	io.on("connection", (socket) => {
 		console.log("client connected");
 
-		socket.on("frontend to backend", (points, who5, sprite, color) => {
-			console.log("frontend to backend", points, who5);
+		socket.on("frontend to backend", (points, who5, sprite, color, age) => {
+			console.log("frontend to backend", who5, sprite, color, age);
+			//if age is less than 18, set who5 to 3
+			if (!age) {
+				who5 = [3,3,3,3,3];
+			}
 			let data = { points: points, who5: who5, sprite: sprite, color: color };
 			client.LPUSH(process.env.REDIS_KEY, JSON.stringify(data));
 		});
